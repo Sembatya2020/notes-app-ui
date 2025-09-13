3c46f61856b8589b4bd32eb11d4d54b99fe21833
Sembatya2020 <ivansmbt@gmail.com>
2025-09-13 19:27:52 +0300

    corrected-app2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders notes management interface', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const addButtonElement = screen.getByText(/add note/i);
+  expect(addButtonElement).toBeInTheDocument();
 });
